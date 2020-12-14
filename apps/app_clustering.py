4b7b32a7d5bfd8bd7d24d9d7f9b99059191ed8ba
Andrew Maguire <andrewm4894@gmail.com>
2020-12-14 22:51:23 +0000

    dev

--- apps/app_clustering.py
+++ apps/app_clustering.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
-from netdata_pandas.data import get_data
+from netdata_pandas.data import get_data, get_chart_list
 from am4894plots.plots import plot_lines, plot_lines_grid
 from netdata_ts_clustering.core import Clusterer
 
@@ -33,7 +34,7 @@ inputs_host = dbc.FormGroup(
 inputs_after = dbc.FormGroup(
     [
         dbc.Label('after', id='label-after', html_for='input-after', style={'margin': '4px', 'padding': '0px'}),
-        dbc.Input(id='input-after', value=-60, type='number', placeholder=-60),
+        dbc.Input(id='input-after', value=-900, type='number', placeholder=-900),
         dbc.Tooltip('"after" as per netdata rest api.', target='label-after')
     ]
 )
@@ -49,7 +50,7 @@ inputs_before = dbc.FormGroup(
 inputs_num_clusters = dbc.FormGroup(
     [
         dbc.Label('k', id='label-num-clusters', html_for='input-num-clusters', style={'margin': '4px', 'padding': '0px'}),
-        dbc.Input(id='input-num-clusters', value=20, type='number', placeholder=20),
+        dbc.Input(id='input-num-clusters', value=3, type='number', placeholder=3),
         dbc.Tooltip('The number of clusters to form.', target='label-num-clusters')
     ]
 )
@@ -71,21 +72,19 @@ tabs = dbc.Tabs(
     ], id='tabs', active_tab='tab-centers', style={'margin': '12px', 'padding': '2px'}
 )
 
-top_card = dbc.CardBody(dcc.Graph(id='fig-clustering'))
-
 layout = html.Div(
     [
         logo,
         main_menu,
         inputs,
         tabs,
-        top_card
+        html.Div(children=html.Div(id='figs')),
     ], style=DEFAULT_STYLE
 )
 
 
 @app.callback(
-    Output('fig-clustering', 'figure'),
+    Output('figs', 'children'),
     Input('btn-run', 'n_clicks'),
     Input('tabs', 'active_tab'),
     State('input-host', 'value'),
@@ -93,19 +92,57 @@ layout = html.Div(
     State('input-before', 'value'),
     State('input-num-clusters', 'value'),
 )
-def display_value(n_clicks, tab, host, after, before, k):
-    #df = get_data([host], ['system.cpu', 'system.load', 'system.net'], after=int(after), before=int(after))
-    model = Clusterer(['london.my-netdata.io'], charts=['system.cpu', 'system.load', 'system.net'], after=-900, before=0, n_clusters=4)
-    model.run_all()
+def get_plots(n_clicks, tab, host, after, before, k):
+    ctx = dash.callback_context
+    print(locals())
+    print(globals())
+    global states
+    global state_change
+    if 'states' not in globals():
+        states = ctx.states
+    if 'state_change' not in globals():
+        state_change = 0
+    for s in ctx.states:
+        if ctx.states[s] != states[s]:
+            state_change += 1
+    if state_change > 0 or 'model' not in globals():
+        states = ctx.states
+        charts_available = get_chart_list(host)
+        global model
+        model = Clusterer([host], charts=charts_available, after=after, before=before, n_clusters=k)
+        model.run_all()
+
+    figs = []
     if tab == 'tab-centers':
-        #fig = plot_lines_grid(df, return_p=True, show_p=False, h=1200)
-        fig = model.fig_centers
+        # plot centers
+        titles = [
+            f'{x[0]} - n={x[2]}, qs={x[1]}'
+            for x in list(
+                zip(
+                    list(model.df_cluster_meta.index),
+                    list(model.df_cluster_meta.quality_score),
+                    list(model.df_cluster_meta.n)
+                )
+            )
+        ]
+        fig_centers = plot_lines_grid(
+            model.df_cluster_centers[list(model.df_cluster_meta.index)],
+            subplot_titles=titles, return_p=True, h_each=300,
+            legend=False, yaxes_visible=False, xaxes_visible=False, show_p=False
+        )
+        figs.append(html.Div(dcc.Graph(id='fig-centers', figure=fig_centers)))
     else:
         # plot clusters
         for cluster in model.df_cluster_meta.index:
-            title = f"Cluster {cluster} (n={model.cluster_len_dict[cluster]}, score = {model.cluster_quality_dict[cluster]})"
+            title = f"Cluster {cluster} (n={model.cluster_len_dict[cluster]}, score={model.cluster_quality_dict[cluster]})"
             plot_cols = model.cluster_metrics_dict[cluster]
-            fig = plot_lines(model.df, cols=plot_cols, title=title, return_p=True, show_p=False)
+            fig_cluster = plot_lines(
+                model.df, cols=plot_cols, title=title, return_p=True, show_p=False,
+                slider=False
+            )
+            figs.append(html.Div(dcc.Graph(id=f'fig-{cluster}', figure=fig_cluster)))
 
-    return fig
+    states_previous = states
+
+    return figs
 

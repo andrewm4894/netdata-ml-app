8b8a80384ea5ab60303256888ce9905be6604a64
Andrew Maguire <andrewm4894@gmail.com>
2021-03-02 21:54:50 +0000

    dev

--- apps/config/config.py
+++ apps/config/config.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import requests
 
 DEFAULT_APP_CONFIG = {
     'hosts': ['london.my-netdata.io', 'newyork.my-netdata.io', 'frankfurt.my-netdata.io']
@@ -14,5 +15,15 @@ def get_config(config_file='app_config.yml'):
     else:
         app_config = DEFAULT_APP_CONFIG
         app_config_found = False
+
+    for host in app_config.get('hosts', []):
+        url = f'http://{host}/api/v1/info'
+        children = requests.get(url).json().get('mirrored_hosts', [])
+        host_children = []
+        for child in children:
+            host_children.append(f'{host}/host/{child}')
+
+    app_config['hosts'].extend(host_children)
+
     return app_config_found, app_config
 

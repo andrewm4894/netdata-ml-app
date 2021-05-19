0c36239b82c3555e5c343c377d44126eae501005
Andrew Maguire <andrewm4894@gmail.com>
2021-05-19 16:05:04 +0100

    build out help docs

---
+++ apps/core/help/defaults.py
@@ -0,0 +1,6 @@
+
+DEFAULT_HELP_INPUTS_HOST = "**host**: the host you want to pull data from."
+DEFAULT_HELP_INPUTS_METRICS = """**metrics**: a string list of the specific metrics you want to focus on in the format "chart.name|metric,chart.name|metric", for example "system.cpu|user,system.load|load1"."""
+DEFAULT_HELP_INPUTS_AFTER = "**after**: `after` as per the [netdata REST API](https://registry.my-netdata.io/swagger/#/default/get_data)."
+DEFAULT_HELP_INPUTS_BEFORE = "**before**: `before` as per the [netdata REST API](https://registry.my-netdata.io/swagger/#/default/get_data)."
+DEFAULT_HELP_INPUTS_OPTIONS = """**options**: a string representing specific `key=val` pairs you can pass to the app, for example "foo=bar,msg=hello" would pass the params `foo=bar` and `msg=hello` to the underlying app."""
--- apps/help/help_metrics_explorer.py
+++ apps/help/help_metrics_explorer.py
@@ -4,16 +4,36 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+from apps.core.help.defaults import DEFAULT_HELP_INPUTS_HOST, DEFAULT_HELP_INPUTS_METRICS, DEFAULT_HELP_INPUTS_AFTER, \
+    DEFAULT_HELP_INPUTS_BEFORE, DEFAULT_HELP_INPUTS_OPTIONS
 from apps.core.utils.logo import logo
 from apps.core.utils.defaults import DEFAULT_STYLE
 
-help_body = """
+help_body = f"""
 ## Metrics Explorer
-Explore a specific set of metrics.
+Explore a specific set of metrics from across multiple charts.  
+#### Inputs
+- {DEFAULT_HELP_INPUTS_HOST}
+- {DEFAULT_HELP_INPUTS_METRICS}
+- {DEFAULT_HELP_INPUTS_AFTER}
+- {DEFAULT_HELP_INPUTS_BEFORE}
+- {DEFAULT_HELP_INPUTS_OPTIONS}
 
-### Inputs
-- **host**: host you want to pull data from?
-- **charts**: a string list of specific metrics you want to focus on in the format "chart.name|metric,chart.name|metric", for example "system.cpu|user,system.load|load1".
+#### Outputs
+There are 3 tabs with results populated by the app.  
+##### Lines  
+Time series plots to see metrics over time on the same plot. There is a line plot with all metrics shown and also a \
+grid plot that shows each metric stacked into a grid of sparklines.   
+##### Scatters  
+Look at scatter plots of all pairs of metrics of interest.  
+##### Histograms  
+Histograms for each metric.  
+
+#### Notes
+- You can use the chart legends to further hide or select metrics of interest.
+- Double click on a metric in the legend to just show that metric.
+- If you are interested in a big window of time then you could use `smooth_n` to smooth the data using a rolling average.
+- The more metrics you include the "busier" the charts will end up looking.
 """
 
 main_menu = dbc.Col(dbc.ButtonGroup(
